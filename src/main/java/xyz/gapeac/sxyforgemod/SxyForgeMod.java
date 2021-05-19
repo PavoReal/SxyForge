d2b6f3e28d1042dcc93336cee2e57e2f12a2e3e9
Garrison Peacock <garrison.peacock@gmail.com>
2021-05-18 23:39:26 -0700

    Bump last commit
    Uploaded wrong version before

--- src/main/java/xyz/gapeac/sxyforgemod/SxyForgeMod.java
+++ src/main/java/xyz/gapeac/sxyforgemod/SxyForgeMod.java
@@ -1,45 +1,41 @@
 package xyz.gapeac.sxyforgemod;
 
-import com.mojang.brigadier.CommandDispatcher;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.network.play.NetworkPlayerInfo;
-import net.minecraft.command.CommandSource;
-import net.minecraft.command.Commands;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.management.PlayerList;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.GameType;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.RenderPlayerEvent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.thread.SidedThreadGroups;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLDedicatedServerSetupEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.event.server.FMLServerStoppingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLEnvironment;
 import net.minecraftforge.fml.network.FMLNetworkConstants;
-import net.minecraftforge.fml.server.ServerLifecycleHooks;
-import net.minecraftforge.server.permission.DefaultPermissionLevel;
+import net.minecraftforge.fml.network.NetworkRegistry;
+import net.minecraftforge.fml.network.PacketDistributor;
+import net.minecraftforge.fml.network.simple.SimpleChannel;
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.jline.utils.Log;
 
 import java.io.*;
+import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.UUID;
 
@@ -48,14 +44,24 @@ public class SxyForgeMod
 {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
-    protected HashMap<UUID, Integer> playerLivesCount;
+
+    public HashMap<UUID, Integer> playerLivesCount;
 
     final String DATA_DIR = "./sxyforge/";
     final String LIVES_BIN_PATH = DATA_DIR + "lives.bin";
 
+    private static final String PROTOCOL_VERSION = "1";
+    public static final SimpleChannel INSTANCE = NetworkRegistry.newSimpleChannel(
+            new ResourceLocation("sxyforgemod", "main"),
+            () -> PROTOCOL_VERSION,
+            PROTOCOL_VERSION::equals,
+            PROTOCOL_VERSION::equals
+    );
+
+    int packetIndex;
+
     public SxyForgeMod()
     {
-        // Register the doClientStuff method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::serverSetup);
 
@@ -64,13 +70,43 @@ public class SxyForgeMod
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
-
         ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
     }
 
     private void commonSetup(FMLCommonSetupEvent event)
     {
+        LOGGER.info("SxyForgeMod presents, slow ass loading by the ForgeTM team");
 
+        packetIndex = 0;
+        INSTANCE.registerMessage(packetIndex++, UUIDLifePair.class, (data, buffer) ->
+        {
+            LOGGER.debug("Encode uuid life pair");
+
+            buffer.writeUUID(data.getKey());
+            buffer.writeInt(data.getValue());
+        }, packetBuffer ->
+                {
+                    UUID id = packetBuffer.readUUID();
+                    Integer lifeCount = packetBuffer.readInt();
+
+                    return new UUIDLifePair(id, lifeCount);
+                },
+                (msg, supplier) ->
+                {
+                    if (supplier.get().getDirection().getReceptionSide().isClient())
+                    {
+                        LOGGER.info("Received new life map");
+
+                        if (msg != null)
+                        {
+                            playerLivesCount.put(msg.getKey(), msg.getValue());
+                        }
+                        else
+                        {
+                            LOGGER.error("Got invalid player lives count");
+                        }
+                    }
+                });
     }
 
     private void serverSetup(FMLDedicatedServerSetupEvent event)
@@ -97,35 +133,36 @@ public class SxyForgeMod
 
     private void clientSetup(final FMLClientSetupEvent event)
     {
-        LOGGER.info("SxyForgeMod says hello");
+        LOGGER.info("The SxyForge mod says hello");
+        playerLivesCount = new HashMap<>();
     }
 
     @SubscribeEvent
     public void onDisplayName(PlayerEvent.NameFormat nameFormatEvent)
     {
-        nameFormatEvent.setDisplayname(new StringTextComponent(TextFormatting.GREEN + nameFormatEvent.getDisplayname().getString()));
+        PlayerEntity player = nameFormatEvent.getPlayer();
 
+        if (playerLivesCount != null && playerLivesCount.containsKey(player.getUUID()))
+        {
+            switch (playerLivesCount.get(player.getUUID()))
+            {
+                case 1:
+                {
+                    nameFormatEvent.setDisplayname(new StringTextComponent(TextFormatting.RED + nameFormatEvent.getDisplayname().getString()));
+                } break;
+
+                case 2:
+                {
+                    nameFormatEvent.setDisplayname(new StringTextComponent(TextFormatting.YELLOW + nameFormatEvent.getDisplayname().getString()));
+                } break;
+
+                case 3:
+                {
+                    nameFormatEvent.setDisplayname(new StringTextComponent(TextFormatting.GREEN + nameFormatEvent.getDisplayname().getString()));
+                } break;
+            }
+        }
     }
-
-//    @SubscribeEvent
-//    public void onCommandsRegister(RegisterCommandsEvent event)
-//    {
-//        LOGGER.info("Registered commands");
-//
-//        event.getDispatcher().register(Commands.literal("reset_all_lives").requires((context) ->
-//        {
-//            return context.hasPermission(0);
-//        }).executes((context) ->
-//        {
-//            PlayerList list = ServerLifecycleHooks.getCurrentServer().getPlayerList();
-//
-//            playerLivesCount.forEach(((uuid, integer) -> {
-//                ServerPlayerEntity player = list.getPlayer(uuid);
-//            }));
-//
-//            return 0;
-//        }));
-//    }
 
     @SubscribeEvent
     public void onServerStopping(FMLServerStoppingEvent event)
@@ -149,60 +186,93 @@ public class SxyForgeMod
     @SubscribeEvent
     public void onDeath(LivingDeathEvent event)
     {
-       if (FMLEnvironment.dist == Dist.DEDICATED_SERVER)
+       if (FMLEnvironment.dist != Dist.DEDICATED_SERVER)
        {
-           Entity e = event.getEntity();
-
-           if (e instanceof PlayerEntity)
-           {
-               PlayerEntity player = (PlayerEntity) e;
-
-               Integer lifeCount = playerLivesCount.get(player.getUUID());
-               lifeCount -= 1;
-               playerLivesCount.replace(player.getUUID(), lifeCount);
-
-               if (lifeCount > 0)
-               {
-                   player.sendMessage(new StringTextComponent(String.format("You have %s lives remaining", lifeCount)), player.getUUID());
-                   LOGGER.info("Minus one life for " + player.getDisplayName().getString());
-               }
-               else
-               {
-                   player.sendMessage(new StringTextComponent("You're out of lives! Setting your gamemode to spectator"), player.getUUID());
-                   player.setGameMode(GameType.SPECTATOR);
-
-                   LOGGER.info(player.getDisplayName().getString() + " has run out of lives!");
-               }
-           }
+           return;
        }
+
+       Entity e = event.getEntity();
+
+        if (e instanceof PlayerEntity)
+        {
+            PlayerEntity player = (PlayerEntity) e;
+
+            if (!playerLivesCount.containsKey(player.getUUID()))
+            {
+                LOGGER.error("Player " + player.getDisplayName().getString() + " doesn't have a life count!");
+                return;
+            }
+
+            Integer lifeCount = playerLivesCount.get(player.getUUID());
+
+            lifeCount -= 1;
+            playerLivesCount.put(player.getUUID(), lifeCount);
+
+            if (lifeCount > 0)
+            {
+                player.sendMessage(new StringTextComponent(String.format("You have %s lives remaining", lifeCount)), player.getUUID());
+                LOGGER.info("Minus one life for " + player.getDisplayName().getString());
+            }
+            else
+            {
+                player.sendMessage(new StringTextComponent("You're out of lives! Setting your gamemode to spectator"), player.getUUID());
+                player.setGameMode(GameType.SPECTATOR);
+
+                LOGGER.info(player.getDisplayName().getString() + " has run out of lives!");
+            }
+
+            LOGGER.info("Sending new life pair to clients");
+            INSTANCE.send(PacketDistributor.ALL.noArg(), new UUIDLifePair(player.getUUID(), lifeCount));
+        }
+    }
+
+    @SubscribeEvent
+    @OnlyIn(Dist.CLIENT)
+    public void renderPlayerPre(RenderPlayerEvent.Pre event)
+    {
+        event.getPlayer().refreshDisplayName();
+    }
+
+    @SubscribeEvent
+    @OnlyIn(Dist.CLIENT)
+    public void renderGameOverlayPre(RenderGameOverlayEvent.Pre event)
+    {
+        if (event.getType() == RenderGameOverlayEvent.ElementType.PLAYER_LIST)
+        {
+
+        }
     }
 
     @SubscribeEvent
     public void playerJoin(PlayerEvent.PlayerLoggedInEvent event)
     {
-        if (FMLEnvironment.dist == Dist.DEDICATED_SERVER)
+        if (FMLEnvironment.dist != Dist.DEDICATED_SERVER)
         {
-            PlayerEntity player = event.getPlayer();
+            return;
+        }
 
-            if (!playerLivesCount.containsKey(player.getUUID()))
+        PlayerEntity player = event.getPlayer();
+
+        if (!playerLivesCount.containsKey(player.getUUID()))
+        {
+            playerLivesCount.put(player.getUUID(), 3);
+
+            player.sendMessage(new StringTextComponent("Welcome! You have three lives left starting now!"), player.getUUID());
+        }
+        else
+        {
+            Integer count = playerLivesCount.get(player.getUUID());
+
+            if (count == 0)
             {
-                playerLivesCount.put(player.getUUID(), 3);
-
-                player.sendMessage(new StringTextComponent("Welcome! You have three lives left starting now!"), player.getUUID());
-            }
-            else
+                player.sendMessage(new StringTextComponent("You're out of lives!"), player.getUUID());
+            } else
             {
-                Integer count = playerLivesCount.get(player.getUUID());
-
-                if (count == 0)
-                {
-                    player.sendMessage(new StringTextComponent("You're out of lives!"), player.getUUID());
-                }
-                else
-                {
-                    player.sendMessage(new StringTextComponent(String.format("Welcome back, you have %s lives", count.toString())), player.getUUID());
-                }
+                player.sendMessage(new StringTextComponent(String.format("Welcome back, you have %s lives", count.toString())), player.getUUID());
             }
         }
+
+        LOGGER.info("Sending new life pair to clients");
+        INSTANCE.send(PacketDistributor.ALL.noArg(), new UUIDLifePair(player.getUUID(), playerLivesCount.get(player.getUUID())));
     }
 }
---
+++ src/main/java/xyz/gapeac/sxyforgemod/UUIDLifePair.java
@@ -0,0 +1,32 @@
+package xyz.gapeac.sxyforgemod;
+
+import javafx.util.Pair;
+
+import java.nio.ByteBuffer;
+import java.util.UUID;
+
+public class UUIDLifePair extends Pair<UUID, Integer>
+{
+    public UUIDLifePair(UUID key, Integer value)
+    {
+        super(key, value);
+    }
+
+    public static byte[] GetBytesFromUUID(UUID id)
+    {
+        ByteBuffer bb = ByteBuffer.wrap(new byte[16]);
+        bb.putLong(id.getMostSignificantBits());
+        bb.putLong(id.getLeastSignificantBits());
+
+        return bb.array();
+    }
+
+    public static UUID GetUUIDFromBytes(byte[] bytes)
+    {
+        ByteBuffer bb = ByteBuffer.wrap(bytes);
+        long first    = bb.getLong();
+        long second   = bb.getLong();
+
+        return new UUID(first, second);
+    }
+}
